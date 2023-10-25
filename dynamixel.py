107d0cc9ca73bb60cba02af7f273e0c18d9bdd65
Matthew Shuttleworth <M.P.Shuttleworth@leeds.ac.uk>
2023-10-25 11:04:09 +0100

    test

--- dynamixel.py
+++ dynamixel.py
@@ -72,12 +72,12 @@ class Dynamixel:
             self.dxl.torque_disable()
             self.dxl.write_control_table("LED", 0)
 
-    def check_movement(self):
-        if self.dxl.read_control_table("Moving") == 1:
-            moving = True
-        else:
-            moving = False
-        return moving
+    # def check_movement(self):
+    #     if self.dxl.read_control_table("Moving") == 1:
+    #         moving = True
+    #     else:
+    #         moving = False
+    #     return moving
 
     def do_move(self, movepos, gcmd):
         movepos = movepos * self.gear_ratio * self.direction
